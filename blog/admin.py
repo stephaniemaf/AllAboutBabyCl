1b35566f920028303563a674b65ca0ac0d4f460b
stephaniemaf <>
2023-12-05 17:03:27 +0000

    added site decoator to register model and class with admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+# use summernotefield for the blog content textfield
+# @ decorater will register post model and post admin class with admin site
+
+@admin.register(Post())
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields("content")
