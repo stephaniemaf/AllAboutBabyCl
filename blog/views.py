01297ae8ce3cb65321192348c31f47a8bf120e27
stephaniemaf <>
2023-12-05 16:19:17 +0000

    added a view for the post list in views.py impoted the post model form models.py to view.py

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).orderby("-pub_date")
+    template_name = "index.html"
+    paginate_by = 6
