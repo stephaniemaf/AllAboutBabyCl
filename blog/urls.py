f8fafa68e94530c5f152749529589788690ae91c
stephaniemaf <>
2023-12-11 15:55:14 +0000

    2

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 # connecting views to url
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
