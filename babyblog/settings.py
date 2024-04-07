59b2e22a4eeff27d2396cb2b7301d54a863b8a86
stephaniemaf <farrell.s.ava@gmail.com>
2024-04-07 15:49:37 +0000

    added new bg color and styled my subscribe page with a background image and moved the form to a better location on the page

--- babyblog/settings.py
+++ babyblog/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['8000-stephaniema-allaboutbab-nkr7143apcb.ws-eu110.gitpod.io',
+ALLOWED_HOSTS = ['8000-stephaniema-allaboutbab-ysllegkxygo.ws-eu110.gitpod.io',
                  'localhost']
 
 
